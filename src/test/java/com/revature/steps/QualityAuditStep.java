76bbaf6e0c2a8d89510273b84982a804dc326c71
Francis Gurahoo <f-gu@prolikewoah.com>
2019-08-27 11:25:31 -0400

    added step implementation, tested out a few things it all works

--- src/test/java/com/revature/steps/QualityAuditStep.java
+++ src/test/java/com/revature/steps/QualityAuditStep.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebDriver;
 import com.revature.pages.QAPage;
 import com.revature.runner.Caliber2Runner;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
 
+@SuppressWarnings("deprecation")
 public class QualityAuditStep {
 
 	public static WebDriver driver = Caliber2Runner.driver;
@@ -19,37 +19,54 @@ public class QualityAuditStep {
 	@Given("^The user is on the Caliber(\\d+) QA page$")
 	public void the_user_is_on_the_Caliber_QA_page(int arg1) throws Throwable {
 	    driver.get("http://localhost:4200/caliber/vp/audit");
+	    Thread.sleep(1000);
 	}
 	
 	//first name might change if you add someone named Aaron for example
 	@Then("^The first trainee is Adams, Gary$")
 	public void the_first_trainee_is_Adams_Gary() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		Assert.assertEquals(qaPage.firstTraineeName, "Adams, Gary");
-		
-	    //throw new PendingException();
+		System.out.println(qaPage.firstTraineeName.getText());
+		Assert.assertEquals("should be adams, gary", 
+				qaPage.firstTraineeName.getText().replaceAll("^\\s+|\\s+$", ""), 
+				"Adams, Gary");
 	}
 
 	@When("^The user clicks on individual feedback$")
 	public void the_user_clicks_on_individual_feedback() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
-		Thread.sleep(2500);
 		qaPage.firstTraineeMenu.click();
+		Thread.sleep(500);
 	}
 
 	@When("^The user clicks on frown$")
 	public void the_user_clicks_on_frown() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
-		Thread.sleep(2500);
 		qaPage.firstTraineeFrown.click();
 	}
 
 	@Then("^The feedback will be frown$")
 	public void the_feedback_will_be_frown() throws Throwable {
-		Thread.sleep(2500);
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
+		Assert.assertTrue(qaPage.firstTraineeMenu.getAttribute("className").contains("fa-frown-o"));
+	}
+	
+	@When("^The user clicks on the first textbox$")
+	public void the_user_clicks_on_the_first_textbox() throws Throwable {
+		qaPage.firstTraineeNotes.click();
+	}
+
+	@When("^The user types hello world$")
+	public void the_user_types_hello_world() throws Throwable {
+		qaPage.firstTraineeNotes.clear();
+		qaPage.firstTraineeNotes.sendKeys("hello world");
+	}
+
+	@Then("^The text feedback will be hello world$")
+	public void the_text_feedback_will_be_hello_world() throws Throwable {
+		Assert.assertEquals("should be hello world", 
+				qaPage.firstTraineeNotes.getText().replaceAll("^\\s+|\\s+$", ""), 
+				"hello world");
+	}
+	
+	@When("^The user hits save$")
+	public void the_user_hits_save() throws Throwable {
+		qaPage.saveButton.click();
 	}
 }
--- src/test/resources/QualityAudit.feature
+++ src/test/resources/QualityAudit.feature
@@ -1,10 +1,15 @@
 Feature: Testing the QA Page
 	Scenario: Ensure the first user is correct
 		Given The user is on the Caliber2 QA page
-		When The user clicks on Quality Audit
 		Then The first trainee is Adams, Gary
 	Scenario: Ensure the user can give feedback emojis
 		When The user clicks on individual feedback
 		When The user clicks on frown
 		Then The feedback will be frown
+	Scenario: Ensure the user can leave text feedback
+		When The user clicks on the first textbox
+		When The user types hello world
+		When The user hits save
+		When The user is on the Caliber2 QA page
+		Then The text feedback will be hello world
 # add more tests as necessary
