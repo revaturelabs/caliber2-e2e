498e74d9d7d17876b975804a838612a05fae6d89
Dillon Mason <d14mason@yahoo.com>
2019-11-13 14:27:13 -0500

    navBar steps

--- src/main/java/com/revature/steps/NavSteps.java
+++ src/main/java/com/revature/steps/NavSteps.java
@@ -1,49 +1,53 @@
 package com.revature.steps;
 
+import org.openqa.selenium.WebDriver;
+
+import com.revature.page.NavigationBar;
+import com.revature.utils.PagesUtil;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.*;
 
 public class NavSteps {
 
-	@Given("^The user is on the Caliber(\\d+) home page$")
+	WebDriver driver = PagesUtil.driver;
+	NavigationBar navBar = PagesUtil.navBar;
+	
+	@Given("^The user is on the Caliber2 home page$")
 	public void the_user_is_on_the_Caliber_home_page(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    driver.get("http://caliber-2-dev.revaturelabs.com.s3-website-us-east-1.amazonaws.com/caliber/vp/home");
 	}
 	
 	@When("^the user clicks on assess batch$")
 	public void the_user_clicks_on_assess_batch() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		navBar.assessbatch.click();
 	}
 	
 	@Given("^User on quality audit page$")
 	public void user_on_quality_audit_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    navBar.qualitybatch.click();
 	}
 	
 	@Given("^The user is on the trainer page$")
 	public void the_user_is_on_the_trainer_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    navBar.settings.click();
+	    navBar.trainers.click();
 	}
 	
 	@Given("^The user is in the Reports tab$")
 	public void the_user_is_in_the_Reports_tab() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    navBar.reports.click();
 	}
 	
 	@Given("^The user is on the caliber (\\d+) location page$")
 	public void the_user_is_on_the_caliber_location_page(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    navBar.settings.click();
+		navBar.locations.click();
 	}
 	
 	@Given("^The user is on the category page$")
 	public void the_user_is_on_the_category_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		navBar.settings.click();
+		navBar.categories.click();
 	}
 }
