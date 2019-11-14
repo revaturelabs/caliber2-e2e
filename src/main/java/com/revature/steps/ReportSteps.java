702951bf7fbf85de63876504e30b616da9eca0e6
maceris <chesburks@gmail.com>
2019-11-14 11:07:06 -0500

    Add more steps, cleanup

--- src/main/java/com/revature/steps/ReportSteps.java
+++ src/main/java/com/revature/steps/ReportSteps.java
@@ -11,12 +11,40 @@ import java.util.List;
 
 public class ReportSteps {
 
-	@When("^The user selects a year from the dropdown for year$")
-	public void the_user_selects_a_year_from_the_dropdown_for_year()
+	@Then("^A week selection drop down menu should be visible$")
+	public void a_week_selection_drop_down_menu_should_be_visible()
 		throws Throwable {
-		PagesUtil.reportsPage.getSelectYearDropdownButton().click();
-		PagesUtil.reportsPage.getDropdownSelectYearContainer()
-			.findElement(By.xpath(".//a[1]")).click();
+		// exception from this will act as a failed assertion
+		WebElement container =
+			PagesUtil.reportsPage.getDropdownSelectWeeksContainer();
+		Assert.assertNotNull(container);
+	}
+
+	@Then("^populated with how many \"([^\"]*)\" they have in that batch$")
+	public void populated_with_how_many_they_have_in_that_batch(
+		String numEntries) throws Throwable {
+
+		int entries = Integer.parseInt(numEntries);
+		final int allWeeksEntry = 1;
+
+		PagesUtil.reportsPage.getSelectWeeksDropdownButton().click();
+		List<WebElement> items = PagesUtil.reportsPage
+			.getDropdownSelectWeeksContainer().findElements(By.tagName("a"));
+		Assert.assertEquals(entries + allWeeksEntry, items.size());
+	}
+
+	@Then("^The batch dropdown should contain items matching the \"([^\"]*)\"$")
+	public void the_batch_dropdown_should_contain_items_matching_the(
+		String criteria) throws Throwable {
+
+		List<WebElement> items = PagesUtil.reportsPage
+			.getDropdownSelectBatchContainer().findElements(By.tagName("a"));
+		for (WebElement item : items) {
+			if (item.getText().contains(criteria)) {
+				return;
+			}
+		}
+		Assert.fail();
 	}
 
 	@Then("^The select batch drop down is populated$")
@@ -26,13 +54,15 @@ public class ReportSteps {
 				.findElements(By.xpath(".//a")).size() > 0);
 	}
 
-	@When("^The user selects a year \"([^\"]*)\"$")
-	public void the_user_selects_a_year(String year) throws Throwable {
-		PagesUtil.reportsPage.getSelectYearDropdownButton().click();
-		PagesUtil.reportsPage.getDropdownSelectYearContainer()
-			.findElement(
-				By.xpath(".//[@id=\"shared-dropdown-menu-" + year + "\"]"))
-			.click();
+	@When("^The user clicks the select batch dropdown$")
+	public void the_user_clicks_the_select_batch_dropdown() throws Throwable {
+		PagesUtil.reportsPage.getBatchSelectDropdownOpenButton().click();
+	}
+
+	@When("^The user inputs a \"([^\"]*)\" into the search bar$")
+	public void the_user_inputs_a_into_the_search_bar(String criteria)
+		throws Throwable {
+		PagesUtil.reportsPage.getBatchSelectSearchBar().sendKeys(criteria);
 	}
 
 	@When("^The user selects a batch \"([^\"]*)\"$")
@@ -58,51 +88,39 @@ public class ReportSteps {
 			.click();
 	}
 
-	@When("^The user clicks the select batch dropdown$")
-	public void the_user_clicks_the_select_batch_dropdown() throws Throwable {
-		PagesUtil.reportsPage.getBatchSelectDropdownOpenButton().click();
+	@When("^The user selects a year \"([^\"]*)\"$")
+	public void the_user_selects_a_year(String year) throws Throwable {
+		PagesUtil.reportsPage.getSelectYearDropdownButton().click();
+		PagesUtil.reportsPage.getDropdownSelectYearContainer()
+			.findElement(
+				By.xpath(".//[@id=\"shared-dropdown-menu-" + year + "\"]"))
+			.click();
 	}
 
-	@When("^The user inputs a \"([^\"]*)\" into the search bar$")
-	public void the_user_inputs_a_into_the_search_bar(String criteria)
+	@When("^The user selects a year from the dropdown for year$")
+	public void the_user_selects_a_year_from_the_dropdown_for_year()
 		throws Throwable {
-		PagesUtil.reportsPage.getBatchSelectSearchBar().sendKeys(criteria);
+		PagesUtil.reportsPage.getSelectYearDropdownButton().click();
+		PagesUtil.reportsPage.getDropdownSelectYearContainer()
+			.findElement(By.xpath(".//a[1]")).click();
 	}
 
-	@Then("^The batch dropdown should contain items matching the \"([^\"]*)\"$")
-	public void the_batch_dropdown_should_contain_items_matching_the(
-		String criteria) throws Throwable {
-
-		List<WebElement> items = PagesUtil.reportsPage
-			.getDropdownSelectBatchContainer().findElements(By.tagName("a"));
-		for (WebElement item : items) {
-			if (item.getText().contains(criteria)) {
-				return;
-			}
-		}
-		Assert.fail();
+	@When("^The user selects all trainees$")
+	public void the_user_selects_all_trainees() throws Throwable {
+		PagesUtil.reportsPage.getSelectTraineesDropdownButton().click();
+		PagesUtil.reportsPage.getDropdownSelectTraineeContainer()
+			.findElement(By
+				.xpath(".//[@id=\"shared-dropdown-menu-Trainees (Overall)\"]"))
+			.click();
 	}
 
-	@Then("^A week selection drop down menu should be visible$")
-	public void a_week_selection_drop_down_menu_should_be_visible()
-		throws Throwable {
-		// exception from this will act as a failed assertion
-		WebElement container =
-			PagesUtil.reportsPage.getDropdownSelectWeeksContainer();
-		Assert.assertNotNull(container);
-	}
-
-	@Then("^populated with how many \"([^\"]*)\" they have in that batch$")
-	public void populated_with_how_many_they_have_in_that_batch(
-		String numEntries) throws Throwable {
-
-		int entries = Integer.parseInt(numEntries);
-		final int allWeeksEntry = 1;
-
+	@When("^The user selects all weeks$")
+	public void the_user_selects_all_weeks() throws Throwable {
 		PagesUtil.reportsPage.getSelectWeeksDropdownButton().click();
-		List<WebElement> items = PagesUtil.reportsPage
-			.getDropdownSelectWeeksContainer().findElements(By.tagName("a"));
-		Assert.assertEquals(entries + allWeeksEntry, items.size());
+		PagesUtil.reportsPage.getDropdownSelectWeeksContainer()
+			.findElement(
+				By.xpath(".//[@id=\"shared-dropdown-menu-All Weeks\"]"))
+			.click();
 	}
 
 }
